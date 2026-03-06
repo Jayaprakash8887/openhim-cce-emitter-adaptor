ddf11a4b4299ee1f5d57bd50857fe4d5b176b07c
jayaprakash8887 <njayaprakash.8887@gmail.com>
2026-03-06 11:15:51 +0530

    Feat #CCE-87: OpenHIM Integration
    Emitter endpoint configuration update

--- src/main/java/org/openphc/cce/emitter/openhim/MediatorRegistrar.java
+++ src/main/java/org/openphc/cce/emitter/openhim/MediatorRegistrar.java
@@ -85,7 +85,7 @@ public class MediatorRegistrar {
      * Builds the {@link MediatorDescriptor} from configuration properties.
      */
     MediatorDescriptor buildDescriptor() {
-        var ep = mediatorProperties.endpoint();
+        var mp = mediatorProperties.endpoint();
 
         return MediatorDescriptor.builder()
                 .urn(mediatorProperties.urn())
@@ -96,11 +96,11 @@ public class MediatorRegistrar {
                 .endpoints(List.of(
                         MediatorDescriptor.Endpoint.builder()
                                 .name(mediatorProperties.name())
-                                .host(ep.host())
-                                .path(ep.path())
+                                .host(mp.host())
+                                .path(mp.path())
                                 .port(serverPort)
                                 .primary(true)
-                                .type(ep.type())
+                                .type(mp.type())
                                 .build()
                 ))
                 .build();
